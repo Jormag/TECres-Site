6e5d7a09e32073fe12bd8f004d58569a9d7733e9
Cristian Marin <cristian-44@hotmail.es>
2019-10-28 16:07:25 -0600

    Rename Insert Usuarios.sql to Data/Insert Usuarios.sql

--- Data/Insert Usuarios.sql
+++ Data/Insert Usuarios.sql
@@ -3,7 +3,7 @@ GO
 
 INSERT INTO [dbo].[Usuario]
            ([Cedula] ,[Nombre] ,[Apellido1],[Apellido2],[FecNaci] ,[Nacionalidad] ,[UbicacionUsuario] ,[Ingresos] ,[Ocupacion] ,[Correo] ,[Perfil], [Usuario],[Contrasena])
-     VALUES (304890267, 'Yendry','Díaz', 'Solís', '1995-09-11', 'costarricense', 245, 20000, 10, 'yenmari11@gmail.com', 1, 'yendiaz', 'ASDFG')
+     VALUES (304890267, 'Yendry','DÃ­az', 'SolÃ­s', '1995-09-11', 'costarricense', 245, 20000, 10, 'yenmari11@gmail.com', 1, 'yendiaz', 'ASDFG')
 
 	 INSERT INTO [dbo].[Usuario]
            ([Cedula] ,[Nombre] ,[Apellido1],[Apellido2],[FecNaci] ,[Nacionalidad] ,[UbicacionUsuario] ,[Ingresos] ,[Ocupacion] ,[Correo] ,[Perfil],[Usuario],[Contrasena])
@@ -11,11 +11,11 @@ INSERT INTO [dbo].[Usuario]
 
 	 INSERT INTO [dbo].[Usuario]
            ([Cedula] ,[Nombre] ,[Apellido1],[Apellido2],[FecNaci] ,[Nacionalidad] ,[UbicacionUsuario] ,[Ingresos] ,[Ocupacion] ,[Correo] ,[Perfil],[Usuario],[Contrasena])
-     VALUES (602170441, 'Alejandro','Díaz', 'Navarro', '1968-08-13', 'costarricense', 247, 500000, 12, 'alediaznavarro@gmail.com', 3, 'alediaz','QWERT')
+     VALUES (602170441, 'Alejandro','DÃ­az', 'Navarro', '1968-08-13', 'costarricense', 247, 500000, 12, 'alediaznavarro@gmail.com', 3, 'alediaz','QWERT')
 
 	 INSERT INTO [dbo].[Usuario]
            ([Cedula] ,[Nombre] ,[Apellido1],[Apellido2],[FecNaci] ,[Nacionalidad] ,[UbicacionUsuario] ,[Ingresos] ,[Ocupacion] ,[Correo] ,[Perfil],[Usuario],[Contrasena])
-     VALUES (305200250, 'Luis','Díaz', 'Solís', '1998-01-13', 'costarricense', 248, 2000000, 13, 'luisdiaz@gmail.com', 1, 'luisdiaz','TYUIO')
+     VALUES (305200250, 'Luis','DÃ­az', 'SolÃ­s', '1998-01-13', 'costarricense', 248, 2000000, 13, 'luisdiaz@gmail.com', 1, 'luisdiaz','TYUIO')
 
 GO
 
